557aa9b49d54fe779ac04fd3c63f1d8dbd28a13f
Meet Merchant <meet.merchant4@gmail.com>
2020-07-25 18:21:17 +0530

    Updated Patient.js

--- models/Patient.js
+++ models/Patient.js
@@ -14,11 +14,16 @@ const mongoose = require('mongoose');
 const PatientSchema = new mongoose.Schema({
         patientID: {
             type: Number,
-        
+            required: true
         },
         name: {
-            
-        }
+            type: String,
+            required: true
+        },
+        disease: {
+            type: String,
+            required: true
+        },
 });
 
 module.exports = Patient = mongoose.model('User', PatientSchema);
