404ece2a711d0ed677ffe5ade594fc6994a75d74
Arsh Shaikh <arsh291206@gmail.com>
2020-07-25 20:38:21 +0530

    login user done

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ app.use(express.json({ extended: false }));
 
 // Route
 app.use('/api/user', require('./routes/api/user'));
+app.use('/api/auth', require('./routes/api/auth'));
 
 const PORT = process.env.PORT || 3000;
 
