ef8a4e0e37be5bcbde1a1bc87a262c3493b7eff6
Arsh Shaikh <arsh291206@gmail.com>
2020-07-25 18:32:24 +0530

    comment structure

--- routes/api/user.js
+++ routes/api/user.js
@@ -1,5 +1,8 @@
 const router = require('express').Router();
 
+// @route    GET api/user
+// @desc     Test api
+// @access   Public
 router.get('/', (req, res) => {
     res.status(400).send('Hi Sir');
 });
