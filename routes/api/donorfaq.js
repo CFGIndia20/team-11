c7e6dccad19473daa48dd56f7fd799533bc8f51f
Arsh Shaikh <arsh291206@gmail.com>
2020-07-26 06:51:24 +0530

    donorfaq added

--- routes/api/donorfaq.js
+++ routes/api/donorfaq.js
@@ -3,6 +3,8 @@ const { check, validationResult } = require('express-validator');
 const DonorFAQ = require('../../models/DonorFAQ');
 const User = require('../../models/User');
 const Center = require('../../models/Center');
+const Patient = require('../../models/Patient');
+const auth = require('../../middlewares/auth');
 
 // @route    GET api/donorfaq
 // @desc     Get Donor FAQ,
@@ -14,29 +16,47 @@ router.post(
         try {
             const user = await User.findById(req.user.id);
 
-            if (user['roleStatus'] !== 'Donor') {
+            console.log(user['roleStatus']);
+
+            if (user['roleStatus'] !== 'DONOR') {
                 return res.status(400).json({
                     errors: [{ msg: 'Access Denied' }],
                 });
             }
 
-            const q = req.body;
+            const { q } = req.body;
 
             switch (q) {
                 case 1:
-                    const center = Center.findOne({ centerCode: user['centerCode']});
+                    const center = await Center.findOne({
+                        centerCode: user['centerCode'],
+                    });
                     return res.status(200).json({ ans: center['capacity'] });
 
                 case 2:
-                    return res.status(200).json({ ans: "John Wick" });
+                    const patients = await Patient.find({
+                        center: user['centerCode'],
+                    });
 
-                case 3:
-                    return res.status(200).json({ ans: 1 });
+                    var list = [];
+
+                    patients.forEach((element) => {
+                        list.push({
+                            name: element['name'],
+                            admission: element['admission'],
+                        });
+                    });
+
+                    return res.status(200).json({ ans: list });
+
+                // case 3:
+
+                //     return res.status(200).json({ ans: 1 });
 
                 default:
                     break;
             }
-            res.status(200).json(newDonorFAQ);
+            // res.status(200).json(newDonorFAQ);
         } catch (err) {
             console.log(err.message);
             res.status(500).send('Server Error');
